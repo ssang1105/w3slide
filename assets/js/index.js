cd4a82187b3c2abf4980041cb9dbd1168762b65e
SangWoo <SW@SangWooui-MacBook-Pro.local>
2014-08-18 00:38:09 +0900

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,9 @@ $(document).ready(function(){
         logo = $('#logo')
     win.load(function(){
         console.log('win.onload')
-        console.log('tset')
-<<<<<<< HEAD
-        console.log('test333333')
-=======
-        console.log('test22')
->>>>>>> 0f023e3ba4e7786281c65e450c61d5eb32b73ba3
         logo.css({
             left:win.innerWidth()/2-logo.width()/2
         })
         btn_signin.css({ left : win.innerWidth()-logo.width()})
-
     });
 });
