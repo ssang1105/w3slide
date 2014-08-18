db564f7b0f719fab925156e8bde935d2c1b50854
ssang1105 <ssang1105@live.co.kr>
2014-08-19 06:28:59 +0900

    good

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     sidebarStatus = false;
     console.log('q')
-    $('.sidebar').click(function() {
+    $('.background').click(function() {
         if (sidebarStatus == false) {
             $('.sidebar').animate({
                 marginLeft: "0px",
@@ -11,8 +11,12 @@ $(document).ready(function() {
                 marginLeft: "270px",
                 opacity: "1"
             }, 500);
-            $('.canvas').animate({
-                marginLeft: "350px",
+//            $('.canvas').animate({
+//                marginLeft: "350px",
+//                opacity: "1"
+//            }, 500);
+            $('.canvas-container').animate({
+                marginLeft: "270px",
                 opacity: "1"
             }, 500);
             sidebarStatus = true;
@@ -26,8 +30,12 @@ $(document).ready(function() {
                 marginLeft: "0px",
                 opacity: "1"
             }, 500);
-            $('.canvas').animate({
-                marginLeft: "220px",
+//            $('.canvas').animate({
+//                marginLeft: "220px",
+//                opacity: "1"
+//            }, 500);
+            $('.canvas-container').animate({
+                marginLeft: "0px",
                 opacity: "1"
             }, 500);
             sidebarStatus = false;
